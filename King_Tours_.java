f22226fe36bf23f02cc5e5601eb31fef99afba2f
greeshma29052005 <147472769+greeshma29052005@users.noreply.github.com>
2024-08-02 21:15:05 +0530

    import java.util.Scanner; public class Demo{     public static void main(String[] args){         int a,b,res;         Scanner read = new Scanner(System.in);         a=read.nextInt();         b=read.nextInt();         res=(5*a)+(7*b);         System.out.println(res);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Demo{
+    public static void main(String[] args){
+        int a,b,res;
+        Scanner read = new Scanner(System.in);
+        a=read.nextInt();
+        b=read.nextInt();
+        res=(5*a)+(7*b);
+        System.out.println(res);
+    }
+}
